252bca7cdc4f8f564c8d7706661582452eec73fc
sota <sota.matsui@akane.waseda.jp>
2025-05-08 17:41:02 +0900

    feat: #6 ナビバーの整備

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { DownloadIcon } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="flex justify-center w-full sticky top-0 border-b border-black/5 px-4 z-10">
+    <header className="flex justify-center w-full sticky top-0 border-b border-white/20 px-4 z-10 backdrop-blur-sm backdrop-saturate-200 bg-gradient-to-b from-black to-transparent">
       <div className="flex w-full max-w-7xl gap-3 justify-between py-4">
         <Link href="/">
           <Image
@@ -18,15 +18,43 @@ export default function Header() {
         </Link>
         <div className="hidden lg:block">
           <div className="flex gap-6 items-center">
-            <div>
-              <Link href="/contact" legacyBehavior passHref>
+            <div className="font-mono">
+              <Link href="/">
                 <div>
-                  お問い合わせ
+                  About
+                </div>
+              </Link>
+            </div>
+            <div className="font-mono">
+              <Link href="/">
+                <div>
+                  Activities
+                </div>
+              </Link>
+            </div>
+            <div className="font-mono">
+              <Link href="/">
+                <div>
+                  Projects
+                </div>
+              </Link>
+            </div>
+            <div className="font-mono">
+              <Link href="/">
+                <div>
+                  Schedules
+                </div>
+              </Link>
+            </div>
+            <div className="font-mono">
+              <Link href="/">
+                <div>
+                  News
                 </div>
               </Link>
             </div>
             <div>
-              <Link href="https://x.com/wasedule">
+              <Link href="https://x.com/">
                 <div>
                   <Image
                     src="/external-assets/x-logo/logo-white.png"
@@ -39,7 +67,7 @@ export default function Header() {
               </Link>
             </div>
             <div>
-              <Link href="https://www.instagram.com/wasedule/">
+              <Link href="https://www.instagram.com/">
                 <div>
                   <Image
                     src="/external-assets/IG_brand_asset_pack_2023/01 Static Glyph/02 White Glyph/Instagram_Glyph_white.svg"
@@ -51,9 +79,11 @@ export default function Header() {
               </Link>
             </div>
             <div>
-              <Link href="/downloads" className="flex gap-1.5 bg-black text-white p-2 rounded-xs">
-                <DownloadIcon />
-                ダウンロード
+              <Link href='' className="flex items-center text-sm">
+                <div className="flex gap-1 bg-foreground text-background p-3">
+                  新入生向け情報
+                  <ArrowRight className="h-5" />
+                </div>
               </Link>
             </div>
           </div>
