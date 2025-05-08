55fc0a267f52ea1242693a9f46561bd758b4ff6a
sota <sota.matsui@akane.waseda.jp>
2025-05-08 13:42:36 +0900

    feat: topページの雛形を実装

---
+++ components/footer.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-center items-center px-4 py-8 gap-8">
+      <section
+        className="flex items-center
+          gap-8 justify-between w-full max-w-7xl mt-16 px-3"
+      >
+        <Link href="">
+          <Image
+            src="/app-team-logo.svg"
+            width={36}
+            height={36}
+            alt="WINC: アプリチーム"
+          />
+        </Link>
+        <div className="flex flex-wrap max-w-7xl gap-8">
+          <Link href="https://x.com/">
+            <Image
+              src="/external-assets/x-logo/logo-black.png"
+              width={31}
+              height={31}
+              alt="公式X - @"
+              className="p-0.5"
+            />
+          </Link>
+          <Link href="https://www.instagram.com/">
+            <Image
+              src="/external-assets/IG_brand_asset_pack_2023/01 Static Glyph/03 Black Glyph/Instagram_Glyph_Black.svg"
+              width={33}
+              height={33}
+              alt="公式Instagram - "
+            />
+          </Link>
+        </div>
+      </section>
+      <section className="flex flex-col items-start max-w-7xl w-full">
+        <Link href="" className="p-3 text-sm font-semibold">
+          プライバシーポリシー
+        </Link>
+        <Link href="" className="p-3 text-sm font-semibold">
+          利用規約
+        </Link>
+      </section>
+      <section className="flex justify-between items-center w-full max-w-7xl text-end py-6 border-t-2 border-gray-100">
+        <div>
+          <Image
+            src="/WINC-logo.svg"
+            width={200}
+            height={36}
+            alt="WINC"
+          />
+          <p className="text-left text-sm text-gray-400 px-3">Created and developed only by waseda students. <br />No other company, organization, or Waseda University is related.</p>
+        </div>
+        <p className="text-sm font-semibold p-3 text-gray-600 w-[-webkit-fill-available] lg:w-auto">© 2025 WINC: App Team</p>
+      </section>
+    </footer>
+  )
+}
---
+++ components/header.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { DownloadIcon } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="flex justify-center w-full sticky top-0 bg-background border-b border-black/5 px-4 z-10">
+      <div className="flex w-full max-w-7xl gap-3 justify-between py-4">
+        <Link href="/">
+          <Image
+            src="/app=team-logo.svg"
+            width={36}
+            height={36}
+            alt="WINCアプリチーム"
+            className="h-6 lg:h-9"
+          />
+        </Link>
+        <div className="hidden lg:block">
+          <div className="flex gap-6 items-center">
+            <div>
+              <Link href="/contact" legacyBehavior passHref>
+                <div>
+                  お問い合わせ
+                </div>
+              </Link>
+            </div>
+            <div>
+              <Link href="https://x.com/wasedule">
+                <div>
+                  <Image
+                    src="/external-assets/x-logo/logo-black.png"
+                    width={24}
+                    height={24}
+                    alt="公式X - @wasedule"
+                    className="p-0.5"
+                  />
+                </div>
+              </Link>
+            </div>
+            <div>
+              <Link href="https://www.instagram.com/wasedule/">
+                <div>
+                  <Image
+                    src="/external-assets/IG_brand_asset_pack_2023/01 Static Glyph/03 Black Glyph/Instagram_Glyph_Black.svg"
+                    width={24}
+                    height={24}
+                    alt="公式Instagram - wasedule"
+                  />
+                </div>
+              </Link>
+            </div>
+            <div>
+              <Link href="/downloads" className="flex gap-1.5 bg-black text-white p-2 rounded-xs">
+                <DownloadIcon />
+                ダウンロード
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
