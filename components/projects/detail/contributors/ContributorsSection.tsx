758b03d331c52b240d8008458407bdbb807a09fb
kyatomaru <souryu1010@gmail.com>
2025-05-11 13:34:09 +0900

    feat: project詳細ページのcms設定

--- components/projects/detail/contributors/ContributorsSection.tsx
+++ components/projects/detail/contributors/ContributorsSection.tsx
@@ -1,13 +1,18 @@
 import ContributorCell from "./contributorCell";
+import { ProjectType } from "@/types/Project";
 
-export default function ContributorsSection() {
+interface ContributorsSectionProps {
+    project: ProjectType;
+}
+
+export default function ContributorsSection({ project }: ContributorsSectionProps) {
     return (
         <div className="flex flex-col gap-2 items-start my-4">
             <h2 className="font-bold text-xs palt">貢献者</h2>
             <div className="w-full flex gap-6 justify-between grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                <ContributorCell name="山田太郎" image="/test-images/members/icon.jpeg" role="山田太郎" />
-                <ContributorCell name="山田太郎" image="/test-images/members/icon.jpeg" role="山田太郎" />
-                <ContributorCell name="山田太郎" image="/test-images/members/icon.jpeg" role="山田太郎" />
+                {project.members.map((member) => (
+                    <ContributorCell key={member.id} member={member} />
+                ))}
             </div>
         </div>
     )
--- components/projects/detail/overview/Overview.tsx
+++ components/projects/detail/overview/Overview.tsx
@@ -1,16 +1,17 @@
-export default function Overview() {
+import { ProjectType } from "@/types/Project";
+
+interface OverviewProps {
+    project: ProjectType;
+}
+
+export default function Overview({ project }: OverviewProps) {
     return (
         <div className="flex flex-col gap-2 items-start my-4">
-            <p className="w-full text-xs palt">
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-                このサイトは、テスト用のサイトです。
-            </p>
+            {project.description.split("\n").map((text, index) => (
+                <p key={index} className="w-full text-xs palt">
+                    {text}
+                </p>
+            ))}
         </div>
     )
 }
