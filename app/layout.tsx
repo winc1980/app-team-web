9623826cb1207ff5ad6f3fbf943f56094ba4be01
sota <sota.matsui@akane.waseda.jp>
2025-05-12 13:10:22 +0900

    feat: 読み込みスクリーンの実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,16 @@ import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Script from "next/script";
+import LoadingOrBg from "@/components/layout/loading-or-bg";
 
 const notoSansJp = Noto_Sans_JP({
   variable: "--font-noto-sans-jp",
+  subsets: ["latin"],
 });
 
 const sans = Poppins({
-  weight: ['400', '500'],
+  weight: ['500'],
+  subsets: ["latin"],
   variable: "--default-sansserif",
 });
 const geistMono = Geist_Mono({
@@ -33,34 +36,15 @@ export default function RootLayout({
       <body
         className={`${notoSansJp.variable} ${geistMono.variable} ${sans.variable} antialiased min-h-screen bg-gradient-to-br from-blue-700 to-rose-700`}
       >
-        <div>
-          <div
-            id="homepage-background"
-            className="fixed h-screen w-screen -z-1 top-0 mix-blend-multiply"
-          ></div>
-          <Script
-            src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-            strategy="beforeInteractive"
-          />
-          <Script
-            src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
-            strategy="beforeInteractive"
-          />
-          <Script id="script">
-            {`VANTA.NET({
-              el: "#homepage-background",
-              mouseControls: true,
-              touchControls: true,
-              gyroControls: false,
-              minHeight: 200.00,
-              minWidth: 200.00,
-              scale: 1.00,
-              scaleMobile: 1.00,
-              color: 0xffffff,
-              backgroundColor: 0x0
-            });`}
-          </Script>
-        </div>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
+          strategy="beforeInteractive"
+        />
+        <LoadingOrBg />
         <Header />
         {children}
         <Footer />
