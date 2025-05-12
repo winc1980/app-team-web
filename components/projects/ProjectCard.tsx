ce1d74fd433fe34daa506cdba05aa3b21cc05836
kyatomaru <souryu1010@gmail.com>
2025-05-12 23:30:57 +0900

    feat: プロジェクト一覧と詳細ページのデザイン変更

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -9,18 +9,35 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
     console.log(project.images);
 
     return (
-        <div className="max-w-xs mt-4 w-full lg:max-w-sm rounded-2xl border border-gray-600 bg-transparent shadow-lg overflow-hidden">
-            <a href={`/projects/${project.id}`} className="inline-block text-blue-300 font-medium">
-                <Image src={project.images[0].url ?? ""} alt={project.title} className="w-full h-40 object-cover" width={100} height={100} />
-                <div className="p-4">
+        <a href={`/projects/${project.id}`} className="w-full flex flex-col md:flex-row font-medium p-4 border border-gray-600 bg-transparent shadow-lg">
+            <div className="md:hidden">
+                <h2 className="text-xl no-underline font-semibold mb-2 text-white">{project.title}</h2>
+            </div>
+
+            <Image src={project.images[0].url ?? ""} className="px-2" alt={project.title} width={150} height={150} />
+            <Image src={project.images[1].url ?? ""} className="px-2" alt={project.title} width={150} height={150} />
+
+            <div className="flex flex-col justify-between items-between px-6 md:block hidden">
+                <div className="flex flex-col gap-16">
                     <h2 className="text-xl no-underline font-semibold mb-2 text-white">{project.title}</h2>
                     <p className="text-sm no-underline text-white">{project.description}</p>
-                    <span className="mt-4 inline-block text-blue-300 hover:underline font-semibold text-sm">
-                        詳しく見る →
+                </div>
+                <div className="flex flex-row justify-end"> 
+                    <span className="mt-4 inline-block text-white-300 hover:underline font-semibold">
+                        MORE →
                     </span>
                 </div>
-            </a>
-        </div>
-
+            </div>
+            <div className="flex flex-col justify-between md:hidden mt-4">  
+                <div className="flex flex-col gap-16">
+                    <p className="text-sm no-underline text-white">{project.description}</p>
+                </div>
+                <div className="flex flex-row justify-end mt-4"> 
+                    <span className="inline-block text-white-300 hover:underline font-semibold text-sm">
+                        MORE →
+                    </span>
+                </div>
+            </div>
+        </a>
       );
 };
